ca7faa7927c98db967f09677f2a4a7f67fa278b2
crucialjun <nich.otieno@gmail.com>
2018-10-30 19:02:37 +0300

    arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+    	double [] leftVals = {100.0d,25.0d,225.0d,11.0d};
+    	double [] rightVals = {50.0d,92.0d,17.0d,3.0d};
+    	char [] opCodes = {'d','a','s','m'};
+    	double[] results = new double[opCodes.length];
 	double val1 = 100.0d,val2 = 50.0d,result;
 	char opCode = 'd';
 
