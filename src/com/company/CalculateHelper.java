93e66218c6710e820b2bd9e132e499095b5a99eb
crucialjun <nich.otieno@gmail.com>
2018-11-21 20:45:48 +0300

    Exceptions

--- src/com/company/CalculateHelper.java
+++ src/com/company/CalculateHelper.java
@@ -8,14 +8,23 @@ public class CalculateHelper {
     MathCommand command;
     double leftValue,rightValue,result;
 
-    public void process(String statement){
+    public void process(String statement) throws InvalidStatementException{
         // add 1.0 2.0
         String[] parts = statement.split(" ");
+        if(parts.length != 3)
+            throw new InvalidStatementException("Incorect number of fields",statement);
+
         String commandString = parts[0]; //add
-        leftValue = Double.parseDouble(parts[1]); //1.0
-        rightValue = Double.parseDouble(parts[2]); //2.0
+        try {
+            leftValue = Double.parseDouble(parts[1]); //1.0
+            rightValue = Double.parseDouble(parts[2]); //2.0
+        }catch (NumberFormatException e){
+            throw new InvalidStatementException("Non-Numeric data", statement,e);
+        }
 
         setCommandFromString(commandString);
+        if(command == null)
+            throw new InvalidStatementException("Invalid command",statement);
 
         CalculateBase calculator = null;
         switch (command){
